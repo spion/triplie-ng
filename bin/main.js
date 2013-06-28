9fe55ff85de6f910f1f1176ce0b99a4503e97a1b
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-06-28 16:16:56 +0200

    fixed bug in triplie --init

--- bin/main.js
+++ bin/main.js
@@ -5,8 +5,8 @@ var args = require('optimist').argv,
     path = require('path');
 
 if (args.init)
-    fs.writeFileSync(path.resolve(process.cwd(), args._[0] || 'config.json'), 
-                     fs.readFileSync(path.join(__dirname,'..','config.json')));
+    fs.writeFileSync(path.resolve(process.cwd(), args._[0] || 'config.yaml'), 
+                     fs.readFileSync(path.join(__dirname,'..','config.yaml')));
 else if (args.feed)
     require('./feed.js');
 else
